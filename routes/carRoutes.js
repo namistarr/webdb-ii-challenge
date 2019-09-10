abf0d6c4b61bf5f87c5484e242cfaea538e27962
namistarr <namistarr@icloud.com>
2019-09-10 13:59:52 -0700

    installed dependencies, added server and routes, GET and POST endpoints in carRoutes.js

---
+++ routes/carRoutes.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const db = require('../data/db-config.js');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    db('cars')
+        .then(cars => {
+            res.status(200).json(cars);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+});
+
+router.get('/:id', (req, res) => {
+    db('cars').where({ id: req.params.id })
+    .first()
+    .then(car => {
+        res.status(200).json(car);
+    })
+    .catch(error => {
+        res.status(500).json(error);
+    })
+});
+
+router.post('/', (req, res) => {
+    const { VIN, make, model, mileage } = req.body;
+
+    if (
+        typeof VIN === 'number'
+        && typeof make === 'string'
+        && typeof model === 'string'
+        && typeof mileage === 'number'
+    ) {
+        db('cars').insert({ VIN, make, model, mileage, transmission, titleStatus }, 'id')
+        .then(([id]) => {
+            res.status(201).json(id);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })        
+    }
+    else {
+        res.status(400).json({
+            message: 'Missing a required field.'
+        })
+    }
+    
+})
+
+module.exports = router;
